78ab03c7f6ca70e55ae100e9d5a65ad1d6624911
Autostructure <bbelanger@azcender.com>
2017-09-26 13:04:38 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 ## site.pp ##
 
-# Here is a new comment
+# Here is a new comment -- XXXXXXX
 
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
